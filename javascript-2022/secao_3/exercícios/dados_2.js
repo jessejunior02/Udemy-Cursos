8c931c101b3d10a09a1119d8fa5962a09c446fc8
Jesse Paiva Carvalho Junior <121308583+jessejunior02@users.noreply.github.com>
2023-01-26 17:30:42 -0300

    exercicio de dados 2

---
+++ javascript-2022/secao_3/exercícios/dados_2.js
@@ -0,0 +1,6 @@
+let precoDoProduto = 19.9;
+let descontoDoProduto = 0.4;
+
+let totalComDesconto = precoDoProduto * (1 - descontoDoProduto);
+
+console.log("O valor do produto é de" + " " + precoDoProduto + " " + ", terá um desconto de" + " " + descontoDoProduto + "%" + " " + "O valor final é" + " " + totalComDesconto + "R$")
