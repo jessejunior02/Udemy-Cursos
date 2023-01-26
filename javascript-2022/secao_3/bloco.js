23df652fdcedc333d8c0498fece19fc7955863af
Jesse Paiva Carvalho Junior <121308583+jessejunior02@users.noreply.github.com>
2023-01-26 14:59:54 -0300

    3 e 4 exercício

---
+++ javascript-2022/secao_3/bloco.js
@@ -0,0 +1,5 @@
+{
+  console.log("Pass 1");
+  console.log("Pass 2");
+  console.log("Pass 3");
+}
---
+++ javascript-2022/secao_3/comentario.js
@@ -0,0 +1,2 @@
+// console.log é usado para exibir algo na tela do computador
+console.log("Hello")
