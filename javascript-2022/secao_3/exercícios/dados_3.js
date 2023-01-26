404b068233b160aa8c9e933270678a46e24e9c10
Jesse Paiva Carvalho Junior <121308583+jessejunior02@users.noreply.github.com>
2023-01-26 17:48:19 -0300

    ex dados 3 feito

---
+++ javascript-2022/secao_3/exercícios/dados_3.js
@@ -0,0 +1,11 @@
+let idadeDeUmaPessoa = 31;
+console.log(typeof idadeDeUmaPessoa); // Number
+
+let salarioDeUmaPessoa = 4578.32;
+console.log(typeof salarioDeUmaPessoa); // Number
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo); // boolean
+
+console.log(typeof "Teste"); // string
+console.log(typeof "Teste"); // string
