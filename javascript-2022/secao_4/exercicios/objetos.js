9c4923b6a95ede26bdb94c40ce75e2356cb11273
Jesse Paiva Carvalho Junior <121308583+jessejunior02@users.noreply.github.com>
2023-02-04 15:18:46 -0300

    Feito 4 aulas

---
+++ javascript-2022/secao_4/exercicios/objetos.js
@@ -0,0 +1,13 @@
+const produtoOne = {};
+produtoOne.nome = "Samsung S20 Ultra"
+produtoOne.preco = 6000.00
+produtoOne['Desconto Legal'] = 0.40 //Evite atributos com espaço
+
+console.log(produtoOne);
+
+const produtoTwo = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+}
+
+console.log(produtoTwo)
---
+++ javascript-2022/secao_4/exercicios/templateString.js
@@ -0,0 +1,12 @@
+const name = "Rebeca";
+const concatenacao = "Ola " + name + "!";
+const template = `
+    Olá ${name}!`;
+
+console.log(concatenacao, template);
+
+//Esprexões...
+console.log(`1 + 1 = ${1 + 1}`);
+
+const up = s => s.toUpperCase()
+console.log(`Ei... ${up('Cuidado')}!`)
